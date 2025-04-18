ba1352b12e161d51b40d752a3636e2e0afade1f8
Bruno Ventura Gross <bvgross@gmail.com>
2025-04-18 16:24:30 -0300

    Primeiros arquivos do projeto final adicionados

---
+++ src/org/serratec/trabalhoFinal/modelos/Pessoa.java
@@ -0,0 +1,14 @@
+package org.serratec.trabalhoFinal.modelos;
+
+public class Pessoa {
+	private String nome;
+	private String cpf;
+	private String senha;
+	
+	public Pessoa(String nome, String cpf, String senha) {
+		this.nome = nome;
+		this.cpf = cpf;
+		this.senha = senha;
+	}
+	
+}
