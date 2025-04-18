93328a998de56b5910710a2717347369a2372439
Letícia Teles <levteles@hotmail.com>
2025-04-18 17:13:20 -0300

    nova classe inserida.

---
+++ src/org/serratec/trabalhoFinal/modelos/Personal.java
@@ -0,0 +1,16 @@
+package org.serratec.trabalhoFinal.modelos;
+
+public class Personal extends Pessoa {
+
+	private String especialidade;
+	private String cref;
+	
+	public Personal(String nome, String cpf, String senha, String especialidade, String cref) {
+		super(nome, cpf, senha);
+		this.especialidade = especialidade;
+		this.cref = cref;
+	}
+	
+	
+	
+}
