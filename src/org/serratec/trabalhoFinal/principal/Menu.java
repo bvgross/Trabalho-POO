4398cfdcc22a12c595bba4a30baddae9f7a2dc67
Victor Campos <victordasilvacampos8991@gmail.com>
2025-04-19 16:57:06 -0300

    Criei o menu de Personal!

--- src/org/serratec/trabalhoFinal/principal/Menu.java
+++ src/org/serratec/trabalhoFinal/principal/Menu.java
@@ -5,7 +5,13 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-import org.serratec.trabalhoFinal.modelos.*;
+import org.serratec.trabalhoFinal.modelos.Aluno;
+import org.serratec.trabalhoFinal.modelos.Avaliacao;
+import org.serratec.trabalhoFinal.modelos.Cargo;
+import org.serratec.trabalhoFinal.modelos.Funcionario;
+import org.serratec.trabalhoFinal.modelos.Personal;
+import org.serratec.trabalhoFinal.modelos.Pessoa;
+import org.serratec.trabalhoFinal.modelos.Plano;
 
 public class Menu {
 	Scanner sc = new Scanner(System.in);
@@ -16,7 +22,7 @@ public class Menu {
 		pessoas.add(new Aluno("Ana", "222", "1234",
 				LocalDate.parse("2025-03-15"), Plano.MENSAL1, "Joãozinho"));
 		pessoas.add(new Personal("Gabriela", "333", "1234", "Spinning", "254-5/6"));
-
+		List<Avaliacao> avaliacoes = new ArrayList<>();
 		boolean estaLogado;
 		System.out.println("===== Academia SerraFit =====\n");
 		do {
@@ -28,7 +34,7 @@ public class Menu {
 			String senha = sc.nextLine();
 
 			//conferindo se esrá estaLogado e se sim iniciando os sub-menus
-			estaLogado = confefirLogin(pessoas, cpf, senha);
+			estaLogado = confefirLogin(pessoas, cpf, senha, avaliacoes);
 			if (!estaLogado) {
 				System.out.println("\nCPF ou senha inválidos.\nDigite novamente abaixo.");
 			}
@@ -38,7 +44,7 @@ public class Menu {
 	/* ----------------------------------
     IMPLEMENTAÇÃO DAS FUNCÕES SEGUNDÁRIAS
     ---------------------------------- */
-	public boolean confefirLogin(List<Pessoa> pessoas, String cpf, String senha) {
+	public boolean confefirLogin(List<Pessoa> pessoas, String cpf, String senha, List<Avaliacao> avaliacoes ) {
 		for (int i = 0; i < pessoas.size(); i++) {
 			if (cpf.equals(pessoas.get(i).getCpf()) && senha.equals(pessoas.get(i).getSenha())) {
 				if (pessoas.get(i).getClass().getSimpleName().equalsIgnoreCase("aluno")) {
@@ -48,7 +54,7 @@ public class Menu {
 					menuFuncionario(pessoas); //sub-menu dos funcionarios
 					return true; 
 				} else {
-					menuPersonal(); //sub-menu dos personais
+					menuPersonal(pessoas, i,  avaliacoes); //sub-menu dos personais
 					return true;
 				}
 			}
@@ -148,7 +154,50 @@ public class Menu {
 	private void calcularFaturamentoMensal() {}
 
 
-	public void menuPersonal() {
-		System.out.println("Personal!!");
+	public void menuPersonal(List<Pessoa> pessoas,int i, List<Avaliacao> avaliacoes) {
+		int opcao;
+		do {
+			System.out.println("""
+					========= Bem vindo Personal! ========= 
+					Digite a opção desejada: 
+					1. Visualizar alunos.
+					2. Registrar avaliações dos alunos.
+					3. Visualizar lista de avaliações.
+					4. Sair.
+							""");
+			opcao = sc.nextInt();
+			sc.nextLine();
+
+			switch (opcao) {
+			case 1 -> visualizarAlunos(pessoas, i);
+			case 2 -> registrarAvaliacao(pessoas, i,avaliacoes);
+			case 3 -> visualizarAvaliacoes(pessoas, i,avaliacoes);
+			case 4 -> System.out.println("Encerrando aplicação...");
+			default -> System.out.println("Opção inválida, digite novamente!");
+
+			}
+			
+		} while(opcao != 4);
 	}
+
+	private void visualizarAvaliacoes(List<Pessoa> pessoas,int i, List<Avaliacao> avaliacoes) {
+		for (Avaliacao avaliacao : avaliacoes) {
+			avaliacao.exibirDados();
+		}
+	}
+
+	private void registrarAvaliacao(List<Pessoa> pessoas,int i, List<Avaliacao> avaliacoes) {
+		System.out.println("Digite o nome do Aluno: ");
+		String nome = sc.nextLine();
+		System.out.println("Digite a descrição da avaliação: ");
+		String descricao = sc.nextLine();
+		avaliacoes.add(new Avaliacao(nome, LocalDate.now(),pessoas.get(i).getNome(), descricao));
+		
+	}
+
+	private void visualizarAlunos(List<Pessoa> pessoas,int i) {
+		/*visuAvAlunos*/
+		
+	}
+	
 }
